39e8c9ba052a2beb66f616d0f643c26a458f4b47
unknown <suryareddy7575@gmail.com>
2025-06-08 23:39:14 +0530

    first commit

---
+++ app.java
@@ -0,0 +1,8 @@
+Hello all
+Welcome to the Java application!
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello all");
+        System.out.println("Welcome to the Java application!");
+    }
+}
